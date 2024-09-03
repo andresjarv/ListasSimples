3341103165bb76fd1272635701fe96af5d84ebf5
sis8 <sis8@confesas.local>
2024-09-03 11:27:50 -0500

    prmer comit del proyecto

---
+++ Menu.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Menu {
+    
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        int tope = 0;
+        int contador = 0;
+
+        ListaEnlazadaSimple lista = new ListaEnlazadaSimple();
+
+
+        System.out.println("Ingrese la cantidad de numeros a almacenar: ");
+        tope = leer.nextInt();
+        while(contador<tope){
+
+
+
+
+            
+            contador++;
+        }
+    }
+    
+}
